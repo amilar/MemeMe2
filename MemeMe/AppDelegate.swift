9d3907c83ebb6a49115b6893e87cf715da5c6532
amilar <github@amilar.be>
2016-05-13 12:10:00 +0200

    Version to submit

--- MemeMe/AppDelegate.swift
+++ MemeMe/AppDelegate.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var origImage: UIImage
+    var memedImage: UIImage
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var memes = [Meme]()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,21 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var topToolbar: UIToolbar!
     @IBOutlet weak var pictureToolbar: UIToolbar!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+
+    // Use a flag to track whether the view has been shifted up for the keyboard
+    var viewShiftedForKeyboard: Bool = false
     
+    // The meme text attributes
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.blackColor(),  // Outline
+        NSForegroundColorAttributeName : UIColor.whiteColor(),  // Fill
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : 0
+    ]
+
+    // ===== Overridden functions =====
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initializeTextFields()
     }
 
     override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // Subscribe to keyboard notifications to allow the view to raise when necessary
+        self.subscribeToKeyboardNotifications()
+        // Enable the camera button if a camera is available
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        // Check whether the Share button should be enabled
+        enableDisableShareButton()
     }
 
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Unsubscribe from keyboard notifications
+        self.unsubscribeFromKeyboardNotifications()
+    }
+    
+    // ===== Keyboard notifications =====
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if ((bottomTextField.isFirstResponder()) && !viewShiftedForKeyboard) {
+            self.view.frame.origin.y -= getKeyboardHeight(notification)
+            viewShiftedForKeyboard = true
+        }
+        else if topTextField.isFirstResponder(){
+            self.view.frame.origin.y = 0
+            viewShiftedForKeyboard = false
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if (bottomTextField.isFirstResponder()) {
+            self.view.frame.origin.y = 0
+            viewShiftedForKeyboard = false
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo!
+        let keyboardSize = userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    // ===== Image picker support =====
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imagePickerView.image = image
@@ -34,6 +99,63 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismissViewControllerAnimated(true, completion: nil)
     }
 
+    // ===== Text field support =====
+    // Initialize the top & bottom text fields to default text and attributes
+    func initializeTextFields() {
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = .Center
+        topTextField.text = "TOP"
+        topTextField.delegate = self
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.textAlignment = .Center
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.delegate = self
+    }
+
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if (textField.text == "TOP" || textField.text == "BOTTOM") {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    // ===== Utility functions =====
+    func enableDisableShareButton() {
+        shareButton.enabled = imagePickerView.image != nil
+    }
+ 
+    // ===== Support functions to create/save meme'd image =====
+    func save() {
+        //Create the meme
+        let meme = Meme( topText: topTextField.text!, bottomText: bottomTextField.text!, origImage:imagePickerView.image!, memedImage: generateMemedImage())
+        
+        (UIApplication.sharedApplication().delegate as!
+            AppDelegate).memes.append(meme)
+    }
+    
+    func generateMemedImage() -> UIImage {
+        // Hide toolbar and navbar
+        topToolbar.hidden = true
+        pictureToolbar.hidden = true
+        
+        // render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        self.view.drawViewHierarchyInRect(self.view.frame, afterScreenUpdates: true)
+        let memedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // Show toolbar and navbar
+        topToolbar.hidden = false
+        pictureToolbar.hidden = false
+
+        return memedImage
+    }
+    
+    // ===== Action handlers =====
     @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -48,5 +170,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
     
+    @IBAction func shareMeme(sender: AnyObject) {
+        let memedImage = generateMemedImage()
+        
+        let avCtl = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        
+        avCtl.completionWithItemsHandler = {
+            activity, completed, items, error in
+            if completed {
+                self.save()
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+        presentViewController(avCtl, animated: true, completion: nil)
+    }
+    
+    @IBAction func resetImage(sender: AnyObject) {
+        imagePickerView.image = nil
+        initializeTextFields()
+        enableDisableShareButton()
+    }
 }
 
